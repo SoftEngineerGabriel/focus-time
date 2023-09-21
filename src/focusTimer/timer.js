7db112627d60593b6f5d9aeaf14b239c6b1fd9ba
SoftEngineerGabriel <farias2752@gmail.com>
2023-09-21 07:00:42 -0300

    corrigindo o acumúlo de timeout

--- src/focusTimer/timer.js
+++ src/focusTimer/timer.js
@@ -1,8 +1,11 @@
 import state from "./state.js"
 import * as el from "./elements.js"
 import { reset } from "./actions.js"
+import { kichenTimer } from "./sounds.js"
 
 export function countdown() {
+
+    clearTimeout(state.countdownId)
 
     if(!state.isRunning){
         return
@@ -22,12 +25,13 @@ export function countdown() {
 
     if(minutes < 0) {
         reset()
+        kichenTimer.play()
         return
     }
  
     updateDisplay(minutes, seconds)
 
-    setTimeout(() => countdown(), 1000)
+    state.countdownId = setTimeout(() => countdown(), 1000)
 
 }
 
